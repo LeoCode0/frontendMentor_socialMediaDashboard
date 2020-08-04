3188ab28e6d376f60714cf556118f7c1c54375ab
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-08-04 16:15:55 -0500

    Creando contexto para el darkmode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 import '../css/Header.css'
 
 export const Header = () => {
+  const [Mode, setMode] = useState(false)
+
+  const handleClick = () => {
+    const Button = document.getElementsByClassName("header__darkMode--button")[0];
+    if (Mode === false){
+      Button.classList.add('active')
+      setMode(true)
+    } else {
+      Button.classList.remove('active')
+      setMode(false)
+    }
+  }
   return (
     <header className="header">
       <h2 className="header__title">Social Media Dashboard</h2>
@@ -10,7 +22,7 @@ export const Header = () => {
       <div className="header__darkMode">
         <span className="header__darkMode--span">Dark Mode</span>
         <div className="header__darkMode--box">
-          <button className="header__darkMode--button"></button>
+          <button className="header__darkMode--button" onClick={handleClick}></button>
         </div>
       </div>
     </header>
